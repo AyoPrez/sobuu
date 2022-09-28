57a5472587a715df0d469a1ff9ef6f6a1149a991
Administrator <admin@example.com>
2022-09-28 15:43:26 +0200

    -added the handling of the errors visually in splash screen

--- app/src/main/java/com/ayoprez/sobuu/presentation/authentication/splash/SplashScreen.kt
+++ app/src/main/java/com/ayoprez/sobuu/presentation/authentication/splash/SplashScreen.kt
@@ -2,8 +2,7 @@ package com.ayoprez.sobuu.presentation.authentication.splash
 
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,15 +15,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ayoprez.sobuu.R
 import com.ayoprez.sobuu.presentation.destinations.HomeScreenDestination
 import com.ayoprez.sobuu.presentation.destinations.LoginScreenDestination
+import com.ayoprez.sobuu.shared.features.authentication.remote.AuthenticationError
 import com.ayoprez.sobuu.shared.features.authentication.remote.AuthenticationResult
-import com.ayoprez.sobuu.ui.theme.DarkLava
-import com.ayoprez.sobuu.ui.theme.GreenSheen
-import com.ayoprez.sobuu.ui.theme.Solway
+import com.ayoprez.sobuu.ui.theme.*
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -36,6 +35,8 @@ fun SplashScreen(
     nav: DestinationsNavigator?,
     viewModel: SplashViewModel = hiltViewModel()
 ) {
+
+    //TODO Add no internet connection detector logic and error message
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -48,28 +49,47 @@ fun SplashScreen(
 
         LaunchedEffect(viewModel, context) {
             viewModel.authResult.collect { result ->
-                when (result) {
-                    is AuthenticationResult.Authorized -> {
-                        nav?.navigate(HomeScreenDestination) {
-                            popUpTo(LoginScreenDestination.route) {
-                                inclusive = true
-                            }
+                if(result is AuthenticationResult.Authorized) {
+                    nav?.navigate(HomeScreenDestination) {
+                        popUpTo(LoginScreenDestination.route) {
+                            inclusive = true
                         }
                     }
-                    is AuthenticationResult.Unauthorized -> {
-                        nav?.navigate(LoginScreenDestination)
-                    }
-                    is AuthenticationResult.Error -> {
-                        viewModel.handleError(result.error)
-                    }
-                    is AuthenticationResult.LoggedOut -> TODO()
-                    is AuthenticationResult.Registered -> TODO()
-                    is AuthenticationResult.ResetPassword -> TODO()
+                } else if (result is AuthenticationResult.Unauthorized) {
+                    nav?.navigate(LoginScreenDestination)
+                } else {
+                    viewModel.handleError(result.error)
                 }
             }
         }
 
-        SplashContent()
+        Column(
+            modifier = Modifier.background(GreenSheen),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            SplashContent()
+
+            if (state.error != null) {
+                Spacer(modifier = Modifier.height(25.dp))
+
+                Text(
+                    getStringFromError(error = state.error),
+                    modifier = Modifier
+                        .padding(start = 32.dp, end = 32.dp),
+                    style = TextStyle(
+                        color = Vermilion,
+                        fontFamily = SourceSans,
+                        fontSize = 20.sp
+                    ),
+                    textAlign = TextAlign.Center,
+                )
+
+                Spacer(modifier = Modifier.height(5.dp))
+            } else {
+                Spacer(modifier = Modifier.height(45.dp))
+            }
+        }
     }
 }
 
@@ -77,7 +97,7 @@ fun SplashScreen(
 fun SplashContent() {
     Box(
         modifier = Modifier.background(GreenSheen),
-        contentAlignment = Alignment.Center
+        contentAlignment = Alignment.Center,
     ) {
         Pulsating(
             pulseFraction = 1.2f
@@ -116,6 +136,17 @@ fun Pulsating(pulseFraction: Float = 1.2f, content: @Composable () -> Unit) {
     }
 }
 
+@Composable
+fun getStringFromError(error: AuthenticationError?): String {
+    return when (error) {
+        is AuthenticationError.TimeOutError -> {
+            stringResource(id = R.string.error_timeout)
+        }
+        else -> {
+            stringResource(id = R.string.error_unknown)
+        }
+    }
+}
 
 @Preview(showSystemUi = true, showBackground = true, group = "Done")
 @Composable
